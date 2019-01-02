8faa3bdbca3d9ac9594f9d3f298d98a93762f224
zhengjiaoyuan <382059276@qq.com>
2019-01-02 20:50:41 +0800

    构建 Client项目
    第一次提交客服端

---
+++ U3dClient/Assets/Scripts/MVCScripts/Controller/LoginCmd/LoginCmd.cs
@@ -0,0 +1,10 @@
+﻿using PureMVC.Interfaces;
+using PureMVC.Patterns;
+
+public class LoginCmd : SimpleCommand
+{
+	public override void Execute(INotification notification)
+	{
+		base.Execute(notification);
+	}
+}
