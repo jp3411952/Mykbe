507fa1052879cbe7037c71bbd896ded1f2874ae3
zhengjiaoyuan <382059276@qq.com>
2019-02-14 00:41:10 +0800

    增加kbeSDK

--- U3dClient/Assets/Scripts/u3d_scripts/Tools/UIEventTriggerListener.cs
+++ U3dClient/Assets/Scripts/u3d_scripts/Tools/UIEventTriggerListener.cs
@@ -1,16 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-namespace mmowar
+/// <summary>
+/// 模仿NGUI实现的
+/// </summary>
+public class UIEventTriggerListener : EventTrigger
 {
-    /// <summary>
-    /// 模仿NGUI实现的
-    /// </summary>
-    public class UIEventTriggerListener : EventTrigger
-{
-
     public delegate void VoidDelegate(PointerEventData eventData);
+
     public delegate void VoidBaseEventDelegate(BaseEventData baseED);
+
     public event VoidDelegate onClick;
     public VoidDelegate onDown;
     public VoidDelegate onEnter;
@@ -19,40 +18,45 @@ namespace mmowar
     public VoidBaseEventDelegate onSelect;
     public VoidBaseEventDelegate onUpdateSelect;
 
-    static public UIEventTriggerListener Get(GameObject go)
+    public static  UIEventTriggerListener Get(GameObject go)
     {
         UIEventTriggerListener listener = go.GetComponent<UIEventTriggerListener>();
         if (listener == null) listener = go.AddComponent<UIEventTriggerListener>();
         return listener;
     }
+
     public override void OnPointerClick(PointerEventData eventData)
     {
         if (onClick != null) onClick(eventData);
     }
+
     public override void OnPointerDown(PointerEventData eventData)
     {
         if (onDown != null) onDown(eventData);
     }
+
     public override void OnPointerEnter(PointerEventData eventData)
     {
         if (onEnter != null) onEnter(eventData);
     }
+
     public override void OnPointerExit(PointerEventData eventData)
     {
         if (onExit != null) onExit(eventData);
     }
+
     public override void OnPointerUp(PointerEventData eventData)
     {
         if (onUp != null) onUp(eventData);
     }
+
     public override void OnSelect(BaseEventData eventData)
     {
         if (onSelect != null) onSelect(eventData);
     }
+
     public override void OnUpdateSelected(BaseEventData eventData)
     {
         if (onUpdateSelect != null) onUpdateSelect(eventData);
     }
-}
-
 }
